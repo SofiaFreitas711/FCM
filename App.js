b8a83e2308f6d3b04e39a29bdace939159999d75
Sofia Freitas <anasofiafreitas711@gmail.com>
2023-01-12 16:31:08 +0000

    imports na app direitos e inicio de editar perfil

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import AuthStack from './src/navigation/AuthStack';
-import AppStack from './src/navigation/AppStack';
+const Stack = createNativeStackNavigator();
 
-function App() {
+import HomePage from './src/screen/HomePage.js';
+import Menu from './src/screen/Menu.js';
+import Onboarding from './src/screen/Onboarding.js';
+import AboutUs from './src/screen/AboutUs.js';
+import Arts from './src/screen/Arts/Arts.js';
+import Art from './src/screen/Arts/Art.js';
+import Artist from './src/screen/Arts/Artist.js';
+import Login from './src/screen/Authenticate/Login.js';
+import Register from './src/screen/Authenticate/Register.js';
+import Games from './src/screen/Games/Games.js';
+import Game from './src/screen/Games/Game.js';
+import Present from './src/screen/News/Present.js';
+import Events from './src/screen/News/Events.js';
+import Event from './src/screen/News/Event.js';
+import News from './src/screen/News/News.js';
+import New from './src/screen/News/New.js';
+import Profile from './src/screen/Profile/Profile.js';
+import EditProfile from './src/screen/Profile/EditProfile.js';
+import Store from './src/screen/Store.js';
+
+const App = () => {
   return (
     <NavigationContainer>
     <Stack.Navigator screenOptions={{headerShown: false}}>
@@ -20,18 +39,18 @@ function App() {
       <Stack.Screen name="Register"component={Register}/>
       <Stack.Screen name="Games"component={Games}/>
       <Stack.Screen name="Game"component={Game}/>
-      <Stack.Screen name="Leaderbord"component={Leaderbord}/>
       <Stack.Screen name="Present"component={Present}/>
       <Stack.Screen name="Events"component={Events}/>
       <Stack.Screen name="Event"component={Event}/>
       <Stack.Screen name="News"component={News}/>
-      <Stack.Screen name="New"component={New}/> */}
-      <Stack.Screen name="Profile"component={Profile}/>
-      {/* <Stack.Screen name="EditProfile"component={EditProfile}/>
-      <Stack.Screen name="Store"component={Store}/> */}
+      <Stack.Screen name="New"component={New}/>
+      <Stack.Screen name="Profile"component={Profile}/> */}
+      <Stack.Screen name="EditProfile"component={EditProfile}/>
+      {/* <Stack.Screen name="Store"component={Store}/> */}
+      
     </Stack.Navigator>
   </NavigationContainer>
   );
-}
+};
 
 export default App;
--- src/screen/Profile/EditProfile.js
+++ src/screen/Profile/EditProfile.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
+  Image,
   StyleSheet,
   Text,
   View,
+  Pressable,
+  Alert,
+  ScrollView,
+  TextInput,
+  TextComponent,
 } from 'react-native';
+import { SvgUri } from 'react-native-svg';
 
 const EditProfile = ({navigation}) => {
+  const user = {'nome': 'César Marques', 'idade': 12, 'localidade': 'Portimão'}
+
+  function saveChanges() {
+    Alert.alert('feito')
+  }
+
   return (
     <View style={styles.titleContainer}>
-      <Text style={styles.title}>Editar Perfil</Text>
+      <SvgUri height='100%' width='100%' uri="https://osithual.sirv.com/Images/FCM/Group%2036.svg" style={styles.bg}/>
+      <Text style={styles.nome}>Olá {user.nome}</Text>
+      <View style={styles.form}>
+        <TextInput placeholder='Username'></TextInput>
+        <TextInput placeholder='Password' secureTextEntry></TextInput>
+        <TextInput placeholder='Confirmação da password' secureTextEntry></TextInput>
+        <TextInput placeholder='Localidade'></TextInput>
+        <Pressable onPress={saveChanges}><Text>Guardar Alterações</Text></Pressable>
+      </View>
     </View>
   )
 }
@@ -16,13 +37,27 @@ const EditProfile = ({navigation}) => {
 const styles = StyleSheet.create({
   titleContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'space-around'
   },
   title: {
     fontSize: 24,
     fontWeight: '600',
     color: 'black',
+  },
+  bg:{
+    position: 'absolute',
+    top:-50,
+    left:0  
+  },
+  nome:{
+    flexDirection:'column',
+    color: '#333333',
+    marginLeft: 210,
+    marginTop: 0,
+    fontWeight: 'bold',
+  },
+  form:{
+    textDecorationLine: 'underline',
   }
 });
 
