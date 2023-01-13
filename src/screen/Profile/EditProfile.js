74724d4596a9a91e753251c023f8f0696e7a378e
Sofia Freitas <anasofiafreitas711@gmail.com>
2023-01-13 12:31:08 +0000

    Editar perfil não concluido

--- src/screen/Profile/EditProfile.js
+++ src/screen/Profile/EditProfile.js
@@ -11,9 +11,15 @@ import {
   TextComponent,
 } from 'react-native';
 import { SvgUri } from 'react-native-svg';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const EditProfile = ({navigation}) => {
   const user = {'nome': 'César Marques', 'idade': 12, 'localidade': 'Portimão'}
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [cPassword, setCPassword] = useState('')
+  const [localidade, setLocalidade] = useState('')
 
   function saveChanges() {
     Alert.alert('feito')
@@ -21,15 +27,34 @@ const EditProfile = ({navigation}) => {
 
   return (
     <View style={styles.titleContainer}>
-      <SvgUri height='100%' width='100%' uri="https://osithual.sirv.com/Images/FCM/Group%2036.svg" style={styles.bg}/>
+      <SvgUri resizeMode="contain" height='100%' width='100%' uri="https://osithual.sirv.com/Images/FCM/Group%2036.svg" style={styles.bg}/>
       <Text style={styles.nome}>Olá {user.nome}</Text>
-      <View style={styles.form}>
-        <TextInput placeholder='Username'></TextInput>
-        <TextInput placeholder='Password' secureTextEntry></TextInput>
-        <TextInput placeholder='Confirmação da password' secureTextEntry></TextInput>
-        <TextInput placeholder='Localidade'></TextInput>
-        <Pressable onPress={saveChanges}><Text>Guardar Alterações</Text></Pressable>
-      </View>
+      <KeyboardAwareScrollView style={styles.form}>
+        <TextInput 
+        placeholder='Username'
+        onChange={setUsername}
+        value={username}></TextInput>
+
+        <TextInput 
+        placeholder='Password'
+        secureTextEntry 
+        onChange={setPassword}
+        value={password}
+        style={styles.form}></TextInput>
+
+        <TextInput
+        placeholder='Confirmação da password'
+        secureTextEntry
+        onChange={setCPassword}
+        value={cPassword}></TextInput>
+
+        <TextInput
+        placeholder='Localidade'
+        onChange={setLocalidade}
+        value={localidade}></TextInput>
+      </KeyboardAwareScrollView>
+
+      <Pressable onPress={saveChanges} style={styles.buttonConfirm}><Text>Guardar Alterações</Text></Pressable>
     </View>
   )
 }
@@ -57,7 +82,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   form:{
-    textDecorationLine: 'underline',
+    position: 'absolute',
+    left: 150
   }
 });
 
