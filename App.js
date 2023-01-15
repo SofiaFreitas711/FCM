6ed2d5bdc0e5f35fae36831d40a38e06f7723722
Sofia Freitas <anasofiafreitas711@gmail.com>
2023-01-15 15:37:17 +0000

    Inicio da galeria

--- App.js
+++ App.js
@@ -31,9 +31,9 @@ const App = () => {
       {/* <Stack.Screen name="HomePage"component={HomePage}/>
       <Stack.Screen name="Menu"component={Menu}/>
       <Stack.Screen name="Onboarding"component={Onboarding}/>
-      <Stack.Screen name="AboutUs"component={AboutUs}/>
+      <Stack.Screen name="AboutUs"component={AboutUs}/> */}
       <Stack.Screen name="Arts"component={Arts}/>
-      <Stack.Screen name="Art"component={Art}/>
+      {/* <Stack.Screen name="Art"component={Art}/>
       <Stack.Screen name="Artist"component={Artist}/>
       <Stack.Screen name="Login"component={Login}/>
       <Stack.Screen name="Register"component={Register}/>
@@ -44,10 +44,10 @@ const App = () => {
       <Stack.Screen name="Event"component={Event}/>
       <Stack.Screen name="News"component={News}/>
       <Stack.Screen name="New"component={New}/>
-      <Stack.Screen name="Profile"component={Profile}/> */}
+      <Stack.Screen name="Profile"component={Profile}/>
       <Stack.Screen name="EditProfile"component={EditProfile}/>
-      {/* <Stack.Screen name="Store"component={Store}/> */}
-      
+      <Stack.Screen name="Store"component={Store}/>
+       */}
     </Stack.Navigator>
   </NavigationContainer>
   );
--- src/screen/Arts/Arts.js
+++ src/screen/Arts/Arts.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
   View,
 } from 'react-native';
+import { SvgUri } from 'react-native-svg';
+import Swiper from 'react-native-swiper';
 
 const Arts = ({navigation}) => {
+  const [sliderId, setSliderId] = useState(0)
+
+  function swipeIndex(id){
+    setSliderId(id)
+  }
+
   return (
     <View style={styles.titleContainer}>
-      <Text style={styles.title}>Obras</Text>
+      <SvgUri uri="https://osithual.sirv.com/Surrealismo/fundo.svg" style={styles.bg}/>
+      <Text style={styles.title}>Surrealismo</Text>
+      <View style={styles.tab}>
+            <Text style={[sliderId == 0 ? styles.tabActive : styles.tabNotActive]}>Obras</Text>
+            <Text style={[sliderId == 1 ? styles.tabActive : styles.tabNotActive]}>Artistas</Text>
+        </View>
+        <Swiper index={0} style={styles.swiper} loop={false} showsPagination={false} onIndexChanged={(idx) => swipeIndex(idx)}>
+            {/* {
+            favoritos.map((item)=>{
+                return(
+                <View style={styles.swiper}>
+                    <Image  style={styles.image} source={{uri:item.obras}}></Image>
+                </View>
+                )
+            })
+            } */}
+            <View>
+                <Text>Artistas</Text>
+            </View>
+            <View>
+                <Text>Noticias</Text>
+            </View>
+        </Swiper>
+      
       {/* <ScrollView
   horizontal={true}
   >
@@ -30,7 +61,31 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '600',
     color: 'black',
-  }
+  },
+  bg: {
+    position: 'absolute',
+    top:0,
+    left:0,
+  },
+  tab:{
+    position: 'absolute',
+    flexDirection:'row',
+    bottom: 490,
+    alignSelf: 'center',
+    color: '#333333',        
+    name:{ 
+        paddingHorizontal:10,           
+        color: '#9697C8'
+    } 
+  },
+  tabNotActive:{
+      paddingHorizontal:10,
+      color: '#9697C8'
+  },
+  tabActive:{
+      paddingHorizontal:10,
+      color: '#2e3192'
+  },
 });
 
 export default Arts;
