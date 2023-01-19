e6b66d7841a9f02a1b407d1504b224d4b7bac93d
Ines <40200335@esmad.ipp.pt>
2023-01-19 19:32:59 +0000

    Pagina evento e noticias done

--- src/screen/News/Event.js
+++ src/screen/News/Event.js
@@ -3,27 +3,95 @@ import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
 
-const Event = ({navigation}) => {
+const New = ({ navigation, route }) => {
+  const item = route.params.event
+
   return (
-    <View style={styles.titleContainer}>
-      <Text style={styles.title}>Evento</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{ uri: item.image }}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
+      <View style={{alignItems: 'center'}}>
+      <Image
+        source={require("../../assets/News/EventsSpecific.png")}
+        style={styles.vector}
+      />
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={styles.description}>{item.type}</Text>
+        <Text style={styles.title}>{item.name}</Text>
+        <Text style={styles.description}>Data  <Text style={styles.date}>24-08-2002</Text></Text>
+      </View>
+      <View style={styles.text}>
+        <Text style={styles.textTitle}>{item.info}</Text>
+      </View>
+
+    </ScrollView>
+
   )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
     flex: 1,
-    alignItems: 'center',
+  },
+  imageContainer: {
     justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     fontSize: 24,
     fontWeight: '600',
     color: 'black',
+  },
+  image: {
+    width: 600,
+    height: 300,
+  },
+  vector: {
+    width: 500,
+    bottom: 180,
+    marginRight: 50,
+  },
+  titleContainer: {
+    alignItems: 'center',
+    marginTop: -250,
+    marginVertical: 10,
+  },
+  description: {
+    color: '#54C5D0',    
+    fontWeight: 'bold',  
+  },
+  title:{
+    fontSize: 40,
+    marginTop: -5,
+    marginBottom: 1,
+    color: '#161616',
+    fontWeight: '600',
+  },
+  textTitle: {
+    fontSize: 16,
+    fontWeight: '400',
+    lineHeight: 26,
+    color: 'black',
+    marginHorizontal: 20,
+    marginVertical: 10,
+    textAlign: 'justify',
+  },
+  date:{ 
+    fontSize: 16,
+    fontWeight: '100',
+    color: 'black',
   }
 });
 
-export default Event;
+export default New;
--- src/screen/News/New.js
+++ src/screen/News/New.js
@@ -8,39 +8,44 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-const New = ({navigation, route}) => {
+const New = ({ navigation, route }) => {
   const item = route.params.new
 
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imageContainer}>
+        <Image
+          source={{ uri: item.image }}
+          style={styles.image}
+        />
+      </View>
+      <View style={{alignItems: 'center'}}>
       <Image
-        source={{uri: item.image}}
-        style={styles.image}
-      />
-      <Image
-        source={require("../../assets/News/EventsSpecific.png")} 
+        source={require("../../assets/News/NewsSpecific.png")}
         style={styles.vector}
       />
-        </View>
+      </View>
       <View style={styles.titleContainer}>
-        <Text style={styles.description}>{item.description}</Text>
+        <Text style={styles.description}>{item.type}</Text>
         <Text style={styles.title}>{item.name}</Text>
+        <Text style={styles.description}>Data: <Text style={styles.date}>24-08-2002</Text></Text>
       </View>
       <View style={styles.text}>
-        <Text style={styles.textTitle}>{item.text}</Text>
-
+        <Text style={styles.textTitle}>{item.info}</Text>
       </View>
+
     </ScrollView>
 
   )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
     flex: 1,
-    alignItems: 'center',
+  },
+  imageContainer: {
     justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     fontSize: 24,
@@ -50,6 +55,39 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: 300,
+  },
+  vector: {
+    width: 500,
+    bottom: 200,
+    marginRight: 50,
+  },
+  titleContainer: {
+    alignItems: 'center',
+    marginTop: -250,
+    marginVertical: 10,
+  },
+  description: {
+    color: '#2E3192',    
+    fontWeight: 'bold',  
+  },
+  title:{
+    fontSize: 40,
+    marginTop: -5,
+    marginBottom: 1,
+    color: '#161616',
+  },
+  textTitle: {
+    fontSize: 16,
+    fontWeight: '400',
+    lineHeight: 26,
+    color: 'black',
+    marginHorizontal: 20,
+    textAlign: 'justify',
+  },
+  date:{
+    fontSize: 16,
+    fontWeight: '100',
+    color: 'black',
   }
 });
 
